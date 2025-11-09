4f827b855e0159016e30ca8452c5c258d99b6cd9
Robert Patterson <robert@robertgpatterson.com>
2025-11-08 20:43:36 -0600

    test for key sig ID and FCExpression.

--- creation_tools/create_property_test.lua
+++ creation_tools/create_property_test.lua
@@ -24,7 +24,7 @@ if not finenv.IsRGPLua then
     ClassNameToFind = returnvalues[1]
     PassedArgument = returnvalues[2]
 else
-    ClassNameToFind = "FCStaffStyleDef"
+    ClassNameToFind = "FCExpression"
     PassedArgument = "obj"
 end
 
